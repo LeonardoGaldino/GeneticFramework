29086cc03c888aa244686f76cef607f82e1620d6
Leonardo Galdino <lcgm@cin.ufpe.br>
2020-09-17 10:41:53 -0300

    Add docstring to learn module

--- src/learn.py
+++ src/learn.py
@@ -1,3 +1,9 @@
+"""Module responsible for the learning process of the eight queen problem.
+
+This module instantiate genetic algorithm framework to find solutions for the
+eight queen problem.
+"""
+
 def main():
     print('Hello, world!')
 
