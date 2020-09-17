213cace14edb15cfd2629932c08e4cf77b01a3b2
LeonardoGaldino <lcgm@cin.ufpe.br>
2020-09-17 16:21:29 -0300

    Add two CLI options: crossover_probability, mutation_probability

--- src/learn.py
+++ src/learn.py
@@ -10,10 +10,15 @@ DEFAULTS = {
     'chess_size': 8,
     'pop_size': 50,
     'max_gen': 50,
+    'crossover_prob': 1.0,
+    'mutation_prob': 0.4,
 }
 
-def main(chess_size: int, population_size: int, max_generations: int):
-    print('Hello, world: {} - {} - {}'.format(chess_size, population_size, max_generations))
+def main(chess_size: int, population_size: int, max_generations: int, 
+        crossover_prob: float, mutation_prob: float):
+    print('Hello, world: {} - {} - {} - {} - {}'
+        .format(chess_size, population_size, max_generations, crossover_prob, 
+            mutation_prob))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=PROGRAM_DESCRIPTION)
@@ -33,5 +38,15 @@ if __name__ == '__main__':
             evolve to find solutions (default={})""".format(DEFAULTS['max_gen']),
         type=int, default=DEFAULTS['max_gen'])
 
+    parser.add_argument('-cp', '--crossover_prob',
+        help="""Specify the probability that a given individual will recombine 
+            (default={})""".format(DEFAULTS['crossover_prob']),
+        type=float, default=DEFAULTS['crossover_prob'])
+
+    parser.add_argument('-mp', '--mutation_prob',
+        help="""Specify the probability that a mutation will occur in a new  
+            individual (default={})""".format(DEFAULTS['mutation_prob']),
+        type=float, default=DEFAULTS['mutation_prob'])
+
     args = parser.parse_args()
-    main(args.chess_size, args.pop_size, args.max_gen)
+    main(args.chess_size, args.pop_size, args.max_gen, args.crossover_prob, args.mutation_prob)
