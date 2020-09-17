8c819f630f0549e1d41c1aff3ec26e7c27785eea
LeonardoGaldino <lcgm@cin.ufpe.br>
2020-09-17 18:23:45 -0300

    Add class for validating positive CLI inputs

--- src/learn.py
+++ src/learn.py
@@ -27,6 +27,17 @@ class CheckProbabilityConstraintAction(Action):
         setattr(namespace, self.dest, values)
 
 
+class CheckPositiveIntegerConstraintAction(Action):
+    """Class responsible for sanitizing positive integers"""
+    
+    def __call__(self, parser, namespace, values, option_string):
+        if values <= 0:
+            raise ValueError(
+                "{} flag has non positive value which is not allowed: {}"
+                    .format(option_string, values))
+        setattr(namespace, self.dest, values)
+
+
 def main(chess_size: int, population_size: int, max_generations: int, 
         crossover_prob: float, mutation_prob: float):
     print('Hello, world: {} - {} - {} - {} - {}'
@@ -39,17 +50,20 @@ if __name__ == '__main__':
     parser.add_argument('-cs', '--chess_size', 
         help="""Specify the size of the chess board in which the puzzle takes 
             place (default={})""".format(DEFAULTS['chess_size']),
-        type=int, default=DEFAULTS['chess_size'])
+        action=CheckPositiveIntegerConstraintAction, type=int,
+        default=DEFAULTS['chess_size'])
 
     parser.add_argument('-ps', '--pop_size', 
         help="""Specify the size of the population that the algorithm will 
             evolve to find solutions (default={})""".format(DEFAULTS['pop_size']),
-        type=int, default=DEFAULTS['pop_size'])
+        action=CheckPositiveIntegerConstraintAction, type=int,
+        default=DEFAULTS['pop_size'])
 
     parser.add_argument('-mg', '--max_gen',
         help="""Specify the maximum number of generations the algorithm should 
             evolve to find solutions (default={})""".format(DEFAULTS['max_gen']),
-        type=int, default=DEFAULTS['max_gen'])
+        action=CheckPositiveIntegerConstraintAction, type=int,
+        default=DEFAULTS['max_gen'])
 
     parser.add_argument('-cp', '--crossover_prob', 
         help="""Specify the probability that a given individual will recombine 
