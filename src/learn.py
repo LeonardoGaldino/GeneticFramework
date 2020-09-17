63d90befbed661a3e7989bf2cb329c803840a0f5
LeonardoGaldino <lcgm@cin.ufpe.br>
2020-09-17 17:46:18 -0300

    Add check for probability CLI inputs range
    Probability input should be float between [0, 1.0]. This commit address this for CLI inputs that are in thi category.

--- src/learn.py
+++ src/learn.py
@@ -3,7 +3,8 @@
 This module instantiate genetic algorithm framework to find solutions for the
 eight queen problem.
 """
-import argparse
+from argparse import ArgumentParser, Action
+
 
 PROGRAM_DESCRIPTION = "Learns eight queens puzzle through genetic algorithm"
 DEFAULTS = {
@@ -14,6 +15,18 @@ DEFAULTS = {
     'mutation_prob': 0.4,
 }
 
+
+class CheckProbabilityConstraintAction(Action):
+    """Class responsible for sanitizing probability CLI inputs' range [0, 1]"""
+    
+    def __call__(self, parser, namespace, values, option_string):
+        if values < 0 or values > 1.0:
+            raise ValueError(
+                "{} flag has a value out of probability boundaries [0, 1.0]: {}"
+                    .format(option_string, values))
+        setattr(namespace, self.dest, values)
+
+
 def main(chess_size: int, population_size: int, max_generations: int, 
         crossover_prob: float, mutation_prob: float):
     print('Hello, world: {} - {} - {} - {} - {}'
@@ -21,7 +34,7 @@ def main(chess_size: int, population_size: int, max_generations: int,
             mutation_prob))
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description=PROGRAM_DESCRIPTION)
+    parser = ArgumentParser(description=PROGRAM_DESCRIPTION)
 
     parser.add_argument('-cs', '--chess_size', 
         help="""Specify the size of the chess board in which the puzzle takes 
@@ -38,15 +51,17 @@ if __name__ == '__main__':
             evolve to find solutions (default={})""".format(DEFAULTS['max_gen']),
         type=int, default=DEFAULTS['max_gen'])
 
-    parser.add_argument('-cp', '--crossover_prob',
+    parser.add_argument('-cp', '--crossover_prob', 
         help="""Specify the probability that a given individual will recombine 
             (default={})""".format(DEFAULTS['crossover_prob']),
-        type=float, default=DEFAULTS['crossover_prob'])
+        action=CheckProbabilityConstraintAction, type=float, 
+        default=DEFAULTS['crossover_prob'])
 
     parser.add_argument('-mp', '--mutation_prob',
         help="""Specify the probability that a mutation will occur in a new  
             individual (default={})""".format(DEFAULTS['mutation_prob']),
-        type=float, default=DEFAULTS['mutation_prob'])
+        action=CheckProbabilityConstraintAction, type=float, 
+        default=DEFAULTS['mutation_prob'])
 
     args = parser.parse_args()
     main(args.chess_size, args.pop_size, args.max_gen, args.crossover_prob, args.mutation_prob)
