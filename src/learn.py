11750ace81595a79efbce4cdc07518a51ffe730e
LeonardoGaldino <lcgm@cin.ufpe.br>
2020-09-17 14:54:13 -0300

    Add CLI options for the algorithm
    Options includes: chess board size, population size, max generations to evolve.

--- src/learn.py
+++ src/learn.py
@@ -3,9 +3,35 @@
 This module instantiate genetic algorithm framework to find solutions for the
 eight queen problem.
 """
+import argparse
 
-def main():
-    print('Hello, world!')
+PROGRAM_DESCRIPTION = "Learns eight queens puzzle through genetic algorithm"
+DEFAULTS = {
+    'chess_size': 8,
+    'pop_size': 50,
+    'max_gen': 50,
+}
+
+def main(chess_size: int, population_size: int, max_generations: int):
+    print('Hello, world: {} - {} - {}'.format(chess_size, population_size, max_generations))
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description=PROGRAM_DESCRIPTION)
+
+    parser.add_argument('-cs', '--chess_size', 
+        help="""Specify the size of the chess board in which the puzzle takes 
+            place (default={})""".format(DEFAULTS['chess_size']),
+        type=int, default=DEFAULTS['chess_size'])
+
+    parser.add_argument('-ps', '--pop_size', 
+        help="""Specify the size of the population that the algorithm will 
+            evolve to find solutions (default={})""".format(DEFAULTS['pop_size']),
+        type=int, default=DEFAULTS['pop_size'])
+
+    parser.add_argument('-mg', '--max_gen',
+        help="""Specify the maximum number of generations the algorithm should 
+            evolve to find solutions (default={})""".format(DEFAULTS['max_gen']),
+        type=int, default=DEFAULTS['max_gen'])
+
+    args = parser.parse_args()
+    main(args.chess_size, args.pop_size, args.max_gen)
