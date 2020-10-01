d5e57c129c18b98b6bd96cfff7941a0cb7d26fef
LeonardoGaldino <lcgm@cin.ufpe.br>
2020-09-30 22:54:26 -0300

    Change BitStringFlipBitMutator to RandomizeGeneMutator

--- src/eight_queens/mutators.py
+++ src/eight_queens/mutators.py
@@ -6,18 +6,18 @@ from genetic_framework.core import *
 from eight_queens.chromosomes import BitStringChromosome
 
 
-class BitStringFlipBitMutator(Mutator[BitStringChromosome]):
+class RandomizeGeneMutator(Mutator[BitStringChromosome]):
 
     @staticmethod
     def mutate(chromosome: BitStringChromosome) -> BitStringChromosome:
         new_chromosome = BitStringChromosome(chromosome.custom_data)
         new_chromosome.data = copy(chromosome.data)
-        BitStringFlipBitMutator.mutate_inplace(new_chromosome)
+        RandomizeGeneMutator.mutate_inplace(new_chromosome)
         return new_chromosome
 
     @staticmethod
     def mutate_inplace(chromosome: BitStringChromosome) -> None:
-        chess_size = BitStringFlipBitMutator.custom_data['chess_size']
+        chess_size = RandomizeGeneMutator.custom_data['chess_size']
 
         gene_index = randint(0, chess_size - 1)
         new_gene_value = randint(0, chess_size - 1)
