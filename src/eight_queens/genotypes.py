71d17c1ff428b635747aa54f4ef78d81fb7fb34c
LeonardoGaldino <lcgm@cin.ufpe.br>
2020-10-02 10:44:55 -0300

    Add IntGenotype to eight_queen genotypes

--- src/eight_queens/genotypes.py
+++ src/eight_queens/genotypes.py
@@ -14,8 +14,8 @@ class BitStringGenotype(Genotype[str]):
 
     def initialize(self) -> None:
         chess_size = self.custom_data['chess_size']
-        column = randint(0, chess_size - 1)
-        self._data = "{:032b}".format(column)
+        row = randint(0, chess_size - 1)
+        self._data = "{:032b}".format(row)
 
     @property
     def data(self) -> str:
@@ -29,6 +29,37 @@ class BitStringGenotype(Genotype[str]):
         if integer_data < 0 or integer_data >= chess_size:
             raise ValueError('Tried to set BitStringGenotype data with ({}). Should be [{}, {}]'
                 .format(integer_data, 0, chess_size - 1))
+
+        self._data = new_data
+    
+    def __str__(self) -> str:
+        return str(self.data)
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+
+class IntGenotype(Genotype[int]):
+
+    def __init__(self, custom_data: Dict = {}) -> None:
+        super().__init__(custom_data)
+        self._data: int = -1
+
+    def initialize(self) -> None:
+        chess_size = self.custom_data['chess_size']
+        self._data = randint(0, chess_size - 1)
+
+    @property
+    def data(self) -> int:
+        return self._data
+
+    @data.setter
+    def data(self, new_data: int) -> None:
+        chess_size = self.custom_data['chess_size']
+
+        if new_data < 0 or new_data >= chess_size:
+            raise ValueError('Tried to set IntGenotype data with ({}). Should be [{}, {}]'
+                .format(new_data, 0, chess_size - 1))
 
         self._data = new_data
     
