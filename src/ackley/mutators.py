b7089c0f46d96768583e003012ae687d94a76232
LeonardoGaldino <lcgm@cin.ufpe.br>
2020-10-19 11:02:06 -0300

    Add mutator to ackley function problem

---
+++ src/ackley/mutators.py
@@ -0,0 +1,29 @@
+from random import gauss
+from copy import deepcopy
+from abc import ABC
+from typing import Type
+
+from genetic_framework.mutator import Mutator
+from ackley.chromosomes import FloatChromosome
+
+
+class IntPermutationSwapGeneRangeMutator(Mutator[FloatChromosome],
+                                         ABC):
+    @classmethod
+    def mutate(cls: Type, chromosome: FloatChromosome) -> FloatChromosome:
+        new_chromosome = deepcopy(chromosome)
+        cls.mutate_inplace(new_chromosome)
+        return new_chromosome
+
+    @classmethod
+    def mutate_inplace(cls: Type, chromosome: FloatChromosome) -> None:
+        step_size: float = cls.custom_data['lower_bound']
+        lower_bound: float = cls.custom_data['upper_bound']
+        upper_bound: float = cls.custom_data['step_size']
+
+        for gene in chromosome.genotypes:
+            delta = gauss(0, step_size)
+            # Avoid moving gene data outside boundaries
+            delta = min(upper_bound - gene.data, max(lower_bound - gene.data, delta))
+            gene.data = gene.data + delta
+
--- src/genetic_framework/mutator.py
+++ src/genetic_framework/mutator.py
@@ -1,4 +1,4 @@
-from typing import Generic
+from typing import Generic, Type
 from abc import ABC, abstractmethod
 
 from genetic_framework.chromosome import ChromosomeT
@@ -15,18 +15,18 @@ class Mutator(Generic[ChromosomeT], CustomDataHolder, ABC):
     Then, mutate and mutate_inplace methods can receive SubClassChromosome 
     safely typechecked.
     """
-    @staticmethod
+    @classmethod
     @abstractmethod
-    def mutate(chromosome: ChromosomeT) -> ChromosomeT:
+    def mutate(cls: Type, chromosome: ChromosomeT) -> ChromosomeT:
         """Mutate a given chromosome into a new one. Subclasses should specify
         the correct type of Chromosome as parameter. 
         (Accordingly to the ChromosomeType specified at the class declaration)
         """
         ...
 
-    @staticmethod
+    @classmethod
     @abstractmethod
-    def mutate_inplace(chromosome: ChromosomeT) -> None:
+    def mutate_inplace(cls: Type, chromosome: ChromosomeT) -> None:
         """Mutate a given chromosome (modifying it, not returning a new one). 
         Subclasses should specify the correct type of Chromosome as parameter. 
         (Accordingly to the ChromosomeType specified at the class declaration)
