d560522cc30223bb80ed17e3d43156fc653f2148
LeonardoGaldino <leonardogaldino96m@gmail.com>
2020-10-18 23:39:12 -0300

    Add genotype for ackley function problem

---
+++ src/ackley/genotypes.py
@@ -0,0 +1,37 @@
+from typing import Dict
+from random import uniform
+
+from genetic_framework.chromosome import Genotype
+
+
+class FloatGenotype(Genotype[float]):
+    def __init__(self, custom_data: Dict = {}) -> None:
+        super().__init__(custom_data)
+        self._data: float = 0.0
+
+    def initialize(self) -> None:
+        lower_bound = self.custom_data['lower_bound']
+        upper_bound = self.custom_data['upper_bound']
+        self._data = uniform(lower_bound, upper_bound)
+
+    @property
+    def data(self) -> float:
+        return self._data
+
+    @data.setter
+    def data(self, new_data: float) -> None:
+        lower_bound = self.custom_data['lower_bound']
+        upper_bound = self.custom_data['upper_bound']
+
+        if new_data < lower_bound or new_data > upper_bound:
+            raise ValueError(
+                'Tried to set FloatGenotype data with ({}). Should be [{}, {}]'
+                .format(new_data, lower_bound, upper_bound))
+
+        self._data = new_data
+
+    def __str__(self) -> str:
+        return str(self.data)
+
+    def __repr__(self) -> str:
+        return self.__str__()
