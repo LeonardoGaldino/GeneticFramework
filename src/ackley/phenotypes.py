14e90067e14c798d51fb247ef489e33b6ca4c8cf
LeonardoGaldino <leonardogaldino96m@gmail.com>
2020-10-18 23:39:51 -0300

    Add phenotype to ackley function problem

---
+++ src/ackley/phenotypes.py
@@ -0,0 +1,31 @@
+from typing import Dict
+
+from genetic_framework.chromosome import Phenotype
+
+
+class FloatPhenotype(Phenotype[float]):
+    def __init__(self, custom_data: Dict = {}) -> None:
+        super().__init__(custom_data)
+        self._data: float = 0.0
+
+    @property
+    def data(self) -> float:
+        return self._data
+
+    @data.setter
+    def data(self, new_data: float) -> None:
+        lower_bound = self.custom_data['lower_bound']
+        upper_bound = self.custom_data['upper_bound']
+
+        if new_data < lower_bound or new_data > upper_bound:
+            raise ValueError(
+                'Tried to set QueenPositionPhenotype data with ({}). Should be [{}, {}]'
+                .format(new_data, lower_bound, upper_bound))
+
+        self._data = new_data
+
+    def __str__(self) -> str:
+        return str(self.data)
+
+    def __repr__(self) -> str:
+        return self.__str__()
