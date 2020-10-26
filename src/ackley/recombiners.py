8b3a84630bc9b03088c141100b79bd730b7562f2
Luca <lams3@cin.ufpe.br>
2020-10-26 15:59:58 -0300

    AdaptiveStepMidPointRecombiner

--- src/ackley/recombiners.py
+++ src/ackley/recombiners.py
@@ -2,7 +2,7 @@ from abc import ABC
 from typing import Type
 
 from genetic_framework.recombiner import Recombiner
-from ackley.chromosomes import FloatChromosome
+from ackley.chromosomes import FloatChromosome, AdaptiveStepFloatChromosome
 
 
 class MidPointRecombiner(Recombiner[FloatChromosome], ABC):
@@ -16,6 +16,27 @@ class MidPointRecombiner(Recombiner[FloatChromosome], ABC):
 
         for i in range(len(new_genes)):
             new_genes[i].data = (genes1[i].data + genes2[i].data) / 2
+
+        new_chromosome.genotypes = new_genes
+        return new_chromosome
+
+
+class AdaptiveStepMidPointRecombiner(Recombiner[AdaptiveStepFloatChromosome],
+                                     ABC):
+    @classmethod
+    def recombine(
+        cls: Type, chromosome1: AdaptiveStepFloatChromosome,
+        chromosome2: AdaptiveStepFloatChromosome
+    ) -> AdaptiveStepFloatChromosome:
+        new_chromosome = AdaptiveStepFloatChromosome(chromosome1.custom_data)
+        new_genes = new_chromosome.genotypes
+        genes1 = chromosome1.genotypes
+        genes2 = chromosome2.genotypes
+
+        for i in range(len(new_genes)):
+            new_value = (genes1[i].data[0] + genes2[i].data[0]) / 2
+            new_delta = (genes1[i].data[1] + genes2[i].data[1]) / 2
+            new_genes[i].data = (new_value, new_delta)
 
         new_chromosome.genotypes = new_genes
         return new_chromosome
