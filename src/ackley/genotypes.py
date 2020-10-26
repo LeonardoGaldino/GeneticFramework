e530044bd1688cd32829a7beba1a89b5ec83c990
Luca <lams3@cin.ufpe.br>
2020-10-26 15:57:34 -0300

    AdaptiveStepFloatGenotype

--- src/ackley/genotypes.py
+++ src/ackley/genotypes.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, Tuple
 from random import uniform
 
 from genetic_framework.chromosome import Genotype
@@ -27,6 +27,41 @@ class FloatGenotype(Genotype[float]):
             raise ValueError(
                 'Tried to set FloatGenotype data with ({}). Should be [{}, {}]'
                 .format(new_data, lower_bound, upper_bound))
+
+        self._data = new_data
+
+    def __str__(self) -> str:
+        return str(self.data)
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+
+class FloatPairGenotype(Genotype[Tuple[float, float]]):
+    def __init__(self, custom_data: Dict = {}) -> None:
+        super().__init__(custom_data)
+        self._data: Tuple[float, float] = (0.0, 0.0)
+
+    def initialize(self) -> None:
+        lower_bound: float = self.custom_data['lower_bound']
+        upper_bound: float = self.custom_data['upper_bound']
+        delta: float = self.custom_data['step_size']
+        x: float = uniform(lower_bound, upper_bound)
+        self._data = (x, delta)
+
+    @property
+    def data(self) -> Tuple[float, float]:
+        return self._data
+
+    @data.setter
+    def data(self, new_data: Tuple[float, float]) -> None:
+        lower_bound: float = self.custom_data['lower_bound']
+        upper_bound: float = self.custom_data['upper_bound']
+
+        if new_data[0] < lower_bound or new_data[0] > upper_bound:
+            raise ValueError(
+                'Tried to set FloatPairGenotype data with ({}). Should be [{}, {}]'
+                .format(new_data[0], lower_bound, upper_bound))
 
         self._data = new_data
 
