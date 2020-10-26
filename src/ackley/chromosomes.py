c158206a72386b9dd8466679a7cb97619c9d3bf5
Luca <lams3@cin.ufpe.br>
2020-10-26 15:58:04 -0300

    AdaptiveStepFloatChromosome

--- src/ackley/chromosomes.py
+++ src/ackley/chromosomes.py
@@ -3,8 +3,8 @@ from random import randint
 from functools import reduce
 from copy import deepcopy
 
-from ackley.phenotypes import FloatPhenotype
-from ackley.genotypes import FloatGenotype
+from ackley.phenotypes import FloatPhenotype, FloatPairPhenotype
+from ackley.genotypes import FloatGenotype, FloatPairGenotype
 from genetic_framework.chromosome import Chromosome
 
 
@@ -56,6 +56,72 @@ class FloatChromosome(Chromosome[FloatPhenotype, FloatGenotype]):
 
     @phenotypes.setter
     def phenotypes(self, phenotypes: List[FloatPhenotype]) -> None:
+        self._genotypes = [
+            self.phenotype_to_genotype(phenotype) for phenotype in phenotypes
+        ]
+
+    def __str__(self) -> str:
+        return str(self._genotypes)
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+
+class AdaptiveStepFloatChromosome(Chromosome[FloatPairPhenotype,
+                                             FloatPairGenotype]):
+    def __init__(self, custom_data: Dict = {}) -> None:
+        super().__init__(custom_data)
+        n: int = self.custom_data['n']
+
+        self._genotypes = [
+            FloatPairGenotype(self.custom_data) for _ in range(n)
+        ]
+
+    def initialize(self) -> None:
+        n: int = self.custom_data['n']
+
+        self._genotypes = [
+            FloatPairGenotype(self.custom_data) for _ in range(n)
+        ]
+        for genotype in self._genotypes:
+            genotype.initialize()
+
+    @staticmethod
+    def genotype_to_phenotype(gene: FloatPairGenotype,
+                              **_) -> FloatPairPhenotype:
+        new_phenotype = FloatPairPhenotype(gene.custom_data)
+        new_phenotype.data = gene.data
+        return new_phenotype
+
+    @staticmethod
+    def phenotype_to_genotype(phenotype: FloatPairPhenotype,
+                              **_) -> FloatPairGenotype:
+        new_gene = FloatPairGenotype(phenotype.custom_data)
+        new_gene.data = phenotype.data
+        return new_gene
+
+    @property
+    def genotypes(self) -> List[FloatPairGenotype]:
+        return self._genotypes
+
+    @genotypes.setter
+    def genotypes(self, genes: List[FloatPairGenotype]) -> None:
+        n: int = self.custom_data['n']
+        if len(genes) != n:
+            raise ValueError(
+                'Tried to assign genotypes to AdaptiveStepFloatChromosome with wrong number of genes ({}). Expected {}.'
+                .format(len(genes), n))
+
+        self._genotypes = deepcopy(genes)
+
+    @property
+    def phenotypes(self) -> List[FloatPairPhenotype]:
+        genes = self._genotypes
+
+        return [self.genotype_to_phenotype(gene) for gene in self._genotypes]
+
+    @phenotypes.setter
+    def phenotypes(self, phenotypes: List[FloatPairPhenotype]) -> None:
         self._genotypes = [
             self.phenotype_to_genotype(phenotype) for phenotype in phenotypes
         ]
