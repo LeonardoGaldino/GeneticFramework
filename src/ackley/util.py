08643c5984f1be7d636f808d81bd2ee9ded6c780
LeonardoGaldino <lcgm@cin.ufpe.br>
2020-10-26 23:29:16 -0300

    Add lerp functions to ackeley problem utils

--- src/ackley/util.py
+++ src/ackley/util.py
@@ -1,2 +1,13 @@
+from random import random
+
+
+def random_lerp(v1: float, v2: float) -> float:
+    return lerp(random(), v1, v2)
+
+
+def lerp(t: float, v1: float, v2: float) -> float:
+    return t * v1 + (1 - t) * v2
+
+
 def clamp(x: float, minimum: float, maximum: float) -> float:
     return max(minimum, min(x, maximum))
