27420976bdeeda1ddbe7bfe57c97dbf69f518a9d
LeonardoGaldino <lcgm@cin.ufpe.br>
2020-10-26 23:31:11 -0300

    Add interpolation to AdaptativeStepMidPointRecombiner

--- src/ackley/recombiners.py
+++ src/ackley/recombiners.py
@@ -1,8 +1,12 @@
 from abc import ABC
 from typing import Type
+from random import gauss
+from math import sqrt
 
+from genetic_framework.fitness import FitnessComputer
 from genetic_framework.recombiner import Recombiner
 from ackley.chromosomes import FloatChromosome, AdaptiveStepFloatChromosome
+from ackley.util import lerp, clamp
 
 
 class MidPointRecombiner(Recombiner[FloatChromosome], ABC):
@@ -24,18 +28,39 @@ class MidPointRecombiner(Recombiner[FloatChromosome], ABC):
 class AdaptiveStepMidPointRecombiner(Recombiner[AdaptiveStepFloatChromosome],
                                      ABC):
     @classmethod
+    def learning_rate(cls: Type) -> float:
+        generation: int = cls.custom_data['generation']
+        lr_multiplier: float = cls.custom_data['learning_rate_multiplier']
+        return lr_multiplier / sqrt(generation)
+
+    @classmethod
     def recombine(
         cls: Type, chromosome1: AdaptiveStepFloatChromosome,
         chromosome2: AdaptiveStepFloatChromosome
     ) -> AdaptiveStepFloatChromosome:
+        lower_bound: float = cls.custom_data['lower_bound']
+        upper_bound: float = cls.custom_data['upper_bound']
+        fitness_computer_cls: Type[FitnessComputer] = cls.custom_data[
+            'fitness_computer']
+        lr = cls.learning_rate()
+
         new_chromosome = AdaptiveStepFloatChromosome(chromosome1.custom_data)
         new_genes = new_chromosome.genotypes
         genes1 = chromosome1.genotypes
         genes2 = chromosome2.genotypes
+        fitness1, fitness2 = fitness_computer_cls.fitness(
+            chromosome1), fitness_computer_cls.fitness(chromosome2)
 
+        diff_fitness = abs(fitness1 - fitness2) / (fitness1 + fitness2)
+
+        t = fitness1 / (fitness1 + fitness2)
+        t += gauss(0, lr * diff_fitness)
         for i in range(len(new_genes)):
-            new_value = (genes1[i].data[0] + genes2[i].data[0]) / 2
-            new_delta = (genes1[i].data[1] + genes2[i].data[1]) / 2
+            new_value = lerp(t, genes1[i].data[0], genes2[i].data[0])
+            new_value = clamp(new_value, lower_bound, upper_bound)
+
+            new_delta = lerp(t, genes1[i].data[1], genes2[i].data[1])
+
             new_genes[i].data = (new_value, new_delta)
 
         new_chromosome.genotypes = new_genes
