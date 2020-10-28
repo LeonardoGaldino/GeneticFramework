387ead44edf994d41c636cdd9f7e155100aa632e
Luca <lams3@cin.ufpe.br>
2020-10-27 21:14:41 -0300

    Preparing FloatGenotype to receive unbounded data

--- src/ackley/genotypes.py
+++ src/ackley/genotypes.py
@@ -1,18 +1,33 @@
 from typing import Dict, Tuple
 from random import uniform
+from enum import Enum
+from math import pi
 
 from genetic_framework.chromosome import Genotype
 
 
 class FloatGenotype(Genotype[float]):
+    class Type(Enum):
+        VARIABLE = 0
+        STEP_SIZE = 1
+        ROTATION_ANGLE = 2
+
     def __init__(self, custom_data: Dict = {}) -> None:
         super().__init__(custom_data)
+        self.type: FloatGenotype.Type = FloatGenotype.Type.VARIABLE
         self._data: float = 0.0
 
     def initialize(self) -> None:
         lower_bound: float = self.custom_data['lower_bound']
         upper_bound: float = self.custom_data['upper_bound']
-        self._data = uniform(lower_bound, upper_bound)
+        step_size: float = self.custom_data['step_size']
+
+        if self.type == FloatGenotype.Type.VARIABLE:
+            self._data = uniform(lower_bound, upper_bound)
+        elif self.type == FloatGenotype.Type.STEP_SIZE:
+            self._data = step_size
+        else:
+            self._data = uniform(0, 2 * pi)
 
     @property
     def data(self) -> float:
@@ -23,7 +38,8 @@ class FloatGenotype(Genotype[float]):
         lower_bound: float = self.custom_data['lower_bound']
         upper_bound: float = self.custom_data['upper_bound']
 
-        if new_data < lower_bound or new_data > upper_bound:
+        if self.type == FloatGenotype.Type.VARIABLE and (
+                new_data < lower_bound or new_data > upper_bound):
             raise ValueError(
                 'Tried to set FloatGenotype data with ({}). Should be [{}, {}]'
                 .format(new_data, lower_bound, upper_bound))
