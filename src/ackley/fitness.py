09703e1a1dbf2a753b0c9eee29b3c33af455aa0b
Luca <lams3@cin.ufpe.br>
2020-10-30 15:42:46 -0300

    Refactoring and small behavior changes

--- src/ackley/fitness.py
+++ src/ackley/fitness.py
@@ -1,24 +1,10 @@
 from typing import Type, List
 from abc import ABC
-from functools import reduce
 from math import cos, exp, sqrt, e
 
 from genetic_framework.fitness import FitnessComputer
 from ackley.chromosomes import FloatChromosome, AdaptiveStepFloatChromosome, CovarianceFloatChromosome
-
-
-def ackley_function(c1: float, c2: float, c3: float,
-                    data: List[float]) -> float:
-    n: int = len(data)
-
-    squares = reduce(lambda acc, value: acc + value * value, data, 0.0)
-    second_sum = reduce(lambda acc, value: acc + cos(c3 * value), data, 0.0)
-
-    result = c1 + e
-    result -= c1 * exp(-c2 * sqrt(1.0 / n) * squares)
-    result -= exp(second_sum / n)
-
-    return result
+from ackley.util import ackley_function
 
 
 class AckleyFitnessComputer(FitnessComputer[FloatChromosome], ABC):
